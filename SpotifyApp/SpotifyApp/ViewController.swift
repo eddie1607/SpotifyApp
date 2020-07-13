b6ba1bf65e225b3948db3ceea52ed836d209f001
eddie1607 <edmilson.info1607@gmail.com>
2020-07-13 15:54:28 -0300

    app clone de spotify - commit  inicial

---
+++ SpotifyApp/SpotifyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyApp
+//
+//  Created by Edmilson vieira da silva on 13/07/20.
+//  Copyright © 2020 Edmilson vieira da silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
